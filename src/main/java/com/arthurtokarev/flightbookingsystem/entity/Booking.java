d0f68d8f9e5d3f8e57291b30a3efd4183e1d7fda
wabys32 <artur.tokarev134@gmail.com>
2026-05-20 16:10:06 +0500

    Added Booking entity for flights booking

---
+++ src/main/java/com/arthurtokarev/flightbookingsystem/entity/Booking.java
@@ -0,0 +1,30 @@
+package com.arthurtokarev.flightbookingsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bookings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime bookingTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_id")
+    private Flight flight;
+}

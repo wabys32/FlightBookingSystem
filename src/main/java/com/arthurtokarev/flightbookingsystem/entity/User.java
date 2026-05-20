788f18c834329a4ae2d2594e17f84e80e5f3305f
wabys32 <artur.tokarev134@gmail.com>
2026-05-20 13:51:13 +0500

    Added UserRepository and User logic. ~Right now I'm building architecture, entity layers and database layers

--- src/main/java/com/arthurtokarev/flightbookingsystem/entity/User.java
+++ src/main/java/com/arthurtokarev/flightbookingsystem/entity/User.java
@@ -1,4 +1,26 @@
 package com.arthurtokarev.flightbookingsystem.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String role;
 }
---
+++ src/main/java/com/arthurtokarev/flightbookingsystem/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.arthurtokarev.flightbookingsystem.repository;
+
+import com.arthurtokarev.flightbookingsystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
